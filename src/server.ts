065eb9fd4473be3b21556ac658b51905f8a660a2
alexandru.bencheci <alexandru.bencheci@gmail.com>
2025-01-21 18:11:28 +0200

    add log level

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import {fastify} from "fastify";
 import path from "path";
 import autoload from "@fastify/autoload";
 
-const app = fastify({logger: true})
+const app = fastify({logger: {level: process.env.LOG_LEVEL ?? 'info'}})
     .register(autoload, {dir: path.join(import.meta.dirname, 'routes')})
 
 await app.listen({port: process.env.API_PORT || 3000});
