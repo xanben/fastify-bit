b45a1a2688eeb3f8bd1d11e72eac4a5fcbf4c235
alexandru.bencheci <alexandru.bencheci@gmail.com>
2025-01-21 17:50:39 +0200

    dynamic load routes from folder

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
 import {fastify} from "fastify";
+import path from "path";
+import autoload from "@fastify/autoload";
 
-const app = fastify({logger: true});
-
-app.get('/', (request, reply) => {
-    reply.send({info: 'Fastify Bit'});
-});
+const app = fastify({logger: true})
+    .register(autoload, {dir: path.join(import.meta.dirname, 'routes')})
 
 await app.listen({port: process.env.API_PORT || 3000});
